8bc9b243295e613a25b2f178ad2a05489458cfb7
陈式超 <1948404397@qq.com>
2017-08-24 19:01:51 +0800

    登录

--- GetHotel/GetHotel/SignInViewController.m
+++ GetHotel/GetHotel/SignInViewController.m
@@ -9,6 +9,9 @@
 #import "SignInViewController.h"
 
 @interface SignInViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordnameTextField;
+- (IBAction)signBtnAction:(UIButton *)sender forEvent:(UIEvent *)event;
 
 @end
 
@@ -16,6 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self naviConfig];
+    [self uiLayout];
     // Do any additional setup after loading the view.
 }
 
@@ -23,7 +28,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+-(void)naviConfig{
+    
+    //设置导航条的颜色（风格颜色）
+    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:20/255.0 green:100/255.0 blue:255.0 alpha:1.0]];
+    //设置导航条的标题颜色
+    self.navigationController.navigationBar.titleTextAttributes=@{NSForegroundColorAttributeName : [UIColor whiteColor] };
+    //设置导航条是否隐藏
+    self.navigationController.navigationBar.hidden=NO;
+    //设置导航条上按钮的风格颜色
+    self.navigationController.navigationBar.tintColor=[UIColor whiteColor];
+    //设置是否需要毛玻璃效果
+    self.navigationController.navigationBar.translucent=YES;
+    //为导航条左上角创建一个按钮
+    UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(backAction)];
+    self.navigationItem.leftBarButtonItem = left;
+}
+//用Model的方式返回上一页
+- (void)backAction {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    //[self.navigationController popViewControllerAnimated:YES];
+}
+-(void)uiLayout{
+    //判断是否存在用户名记忆体
+    if (![[Utilities getUserDefaults:@"Username"] isKindOfClass:[NSNull class]]) {
+        if ([Utilities getUserDefaults:@"Username"] != nil) {
+            //将他显示在用户名输入框
+            _usernameTextField.text=[Utilities getUserDefaults:@"Username"];
+        }
+    }
+}
 /*
 #pragma mark - Navigation
 
@@ -34,4 +68,40 @@
 }
 */
 
+- (IBAction)signBtnAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    if (_usernameTextField.text.length==0) {
+        [Utilities popUpAlertViewWithMsg:@"请输入你的手机号" andTitle:nil onView:self];
+        return;
+    }
+    //判断某个字符串中是否每个字符都是数字
+    NSCharacterSet *notDigits=[[NSCharacterSet decimalDigitCharacterSet]invertedSet];
+    if ([_usernameTextField.text rangeOfCharacterFromSet:notDigits].location != NSNotFound || _usernameTextField.text.length != 11) {
+        [Utilities popUpAlertViewWithMsg:@"请输入有效的手机号码" andTitle:nil onView:self];
+        return;
+    }
+    
+    if (_passwordnameTextField.text.length==0) {
+        [Utilities popUpAlertViewWithMsg:@"请输入密码" andTitle:nil onView:self];
+        return;
+    }
+    if (_passwordnameTextField.text.length < 6 || _passwordnameTextField.text.length > 18) {
+        [Utilities popUpAlertViewWithMsg:@"你输入的密码必须在6~18之间" andTitle:nil onView:self];
+        return;
+    }
+    //无输入异常的情况，开始正式执行登录接口
+   // [self readyForEncoding];
+    
+}
+//键盘收回
+- (void) touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //让根视图结束编辑状态达到收起键盘的目的
+    [self.view endEditing:YES];
+}
+//按return收回键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if (textField == _usernameTextField || textField == _passwordnameTextField) {
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
 @end
--- GetHotel/GetHotel/SignUpViewController.m
+++ GetHotel/GetHotel/SignUpViewController.m
@@ -9,6 +9,10 @@
 #import "SignUpViewController.h"
 
 @interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userTelTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passWordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmTextField;
+- (IBAction)signUpAction:(UIButton *)sender forEvent:(UIEvent *)event;
 
 @end
 
@@ -16,6 +20,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self naviConfig];
+    [self uiLayout];
     // Do any additional setup after loading the view.
 }
 
@@ -23,7 +29,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+-(void)naviConfig{
+    
+    //设置导航条的颜色（风格颜色）
+    [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:20/255.0 green:100/255.0 blue:255.0 alpha:1.0]];
+    //设置导航条的标题颜色
+    self.navigationController.navigationBar.titleTextAttributes=@{NSForegroundColorAttributeName : [UIColor whiteColor] };
+    //设置导航条是否隐藏
+    self.navigationController.navigationBar.hidden=NO;
+    //设置导航条上按钮的风格颜色
+    self.navigationController.navigationBar.tintColor=[UIColor whiteColor];
+    //设置是否需要毛玻璃效果
+    self.navigationController.navigationBar.translucent=YES;
+    //为导航条左上角创建一个按钮
+    UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(backAction)];
+    self.navigationItem.leftBarButtonItem = left;
+}
+//用Model的方式返回上一页
+- (void)backAction {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    //[self.navigationController popViewControllerAnimated:YES];
+}
+-(void)uiLayout{
+    //判断是否存在用户名记忆体
+    if (![[Utilities getUserDefaults:@"Username"] isKindOfClass:[NSNull class]]) {
+        if ([Utilities getUserDefaults:@"Username"] != nil) {
+            //将他显示在用户名输入框
+            _userTelTextField.text=[Utilities getUserDefaults:@"Username"];
+        }
+    }
+}
 /*
 #pragma mark - Navigation
 
@@ -34,4 +69,43 @@
 }
 */
 
+- (IBAction)signUpAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    if (_userTelTextField.text.length== 0) {
+        [Utilities popUpAlertViewWithMsg:@"请输入你的手机号" andTitle:nil onView:self];
+        return;
+    }
+    //判断某个字符串中是否每个字符都是数字
+    NSCharacterSet *notDigits=[[NSCharacterSet decimalDigitCharacterSet]invertedSet];
+    if ([_userTelTextField.text rangeOfCharacterFromSet:notDigits].location != NSNotFound || _userTelTextField.text.length != 11) {
+        [Utilities popUpAlertViewWithMsg:@"请输入有效的手机号码" andTitle:nil onView:self];
+        return;
+    }
+    if (_passWordTextField.text.length==0) {
+        [Utilities popUpAlertViewWithMsg:@"请输入密码" andTitle:nil onView:self];
+        return;
+    }
+    if (_passWordTextField.text.length < 6 || _passWordTextField.text.length > 18) {
+        [Utilities popUpAlertViewWithMsg:@"你输入的密码必须在6~18之间" andTitle:nil onView:self];
+        return;
+    }
+    if ([_passWordTextField.text isEqualToString:_confirmTextField.text]) {
+        // [self request];
+    }else{
+        [Utilities popUpAlertViewWithMsg:@"密码输入不一致，请重新输入" andTitle:@"提示" onView:self];
+        // _passWordTextField.text = @"";
+        _confirmTextField.text = @"";
+    }
+}
+//键盘收回
+- (void) touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    //让根视图结束编辑状态达到收起键盘的目的
+    [self.view endEditing:YES];
+}
+//按return收回键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if (textField == _userTelTextField || textField == _passWordTextField || textField == _confirmTextField ) {
+        [textField resignFirstResponder];
+    }
+    return YES;
+}
 @end
