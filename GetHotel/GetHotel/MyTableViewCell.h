71345b997b2df440c5925fa28d1169dcf7a2ccdf
陈永剑 <1776968161@qq.com>
2017-08-24 18:56:50 +0800

    我的

---
+++ GetHotel/GetHotel/MyTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  MyTableViewCell.h
+//  GetHotel
+//
+//  Created by admin on 2017/8/24.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *htImage;
+@property (weak, nonatomic) IBOutlet UILabel *hotelsLabel;
+
+@end
