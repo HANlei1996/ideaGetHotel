8b28618b65eb6eb35e896dfa7d07146b622551b9
何伦 <938788163@qq.com>
2017-08-26 16:47:30 +0800

    MY

---
+++ GetHotel/GetHotel/HotelsViewTableCellTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  HotelsViewTableCellTableViewCell.m
+//  GetHotel
+//
+//  Created by admin on 2017/8/26.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import "HotelsViewTableCellTableViewCell.h"
+
+@implementation HotelsViewTableCellTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
