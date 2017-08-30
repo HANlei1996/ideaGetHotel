0a0545610af3fa4346962e42f8cc9c6cdd2e1aa6
陈式超 <1948404397@qq.com>
2017-08-30 16:26:01 +0800

    接口

--- GetHotel/GetHotel/UserModel.m
+++ GetHotel/GetHotel/UserModel.m
@@ -15,12 +15,12 @@
         
         _memberId=[Utilities nullAndNilCheck:dict[@"memberId"] replaceBy:@"0"];
         _phone=[Utilities nullAndNilCheck:dict[@"contactTel"] replaceBy:@"未设置"];
-        _nickname=[Utilities nullAndNilCheck:dict[@"memberName"] replaceBy:@"未设置"];
+        _nickname=[Utilities nullAndNilCheck:dict[@"nick_name"] replaceBy:@"未设置"];
         _age=[Utilities nullAndNilCheck:dict[@"age"] replaceBy:@"0"];
         _dob=[Utilities nullAndNilCheck:dict[@"birthday"] replaceBy:@"未设置"];
         _idCardNo=[Utilities nullAndNilCheck:dict[@"identificationcard"] replaceBy:@"未设置"];
         _credit=[Utilities nullAndNilCheck:dict[@"memberPoint"] replaceBy:@"0"];
-        _avatarUrl=[Utilities nullAndNilCheck:dict[@"memberUrl"] replaceBy:@""];
+        _avatarUrl=[Utilities nullAndNilCheck:dict[@"head_img"] replaceBy:@""];
    //     _tokenKey=[Utilities nullAndNilCheck:dict[@"key"] replaceBy:@""];
         if ([dict[@"memberSex"] isKindOfClass:[NSNull class]]) {
             _gender=@"";
